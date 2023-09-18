b7086462bb24eef2fe0ae2467eae40815af542bc
Mateus Gomes <gmateus987@gmail.com>
2023-09-18 11:53:49 -0300

    Last updates - Finalizing the website

--- js/music-player.js
+++ js/music-player.js
@@ -1,11 +1,16 @@
 // LIKE BUTTON
-
-const likeButton = document.getElementById('likeButton');
-let liked = false;
-
-likeButton.addEventListener('click', () => {
-    liked = !liked;
-    likeButton.classList.toggle('liked', liked);
-})
+let icon = document.querySelector('ion-icon');
+icon.onclick = function(){
+    icon.classList.toggle('active');
+}
 
 // PLAYER
+let currentTime = document.getElementById('label-time');
+let rangeInput = document.getElementById('music-time');
+
+currentTime.innerText = rangeInput.value;
+
+rangeInput.oninput = function(){
+    currentTime.innerHTML = parseInt(this.value).to
+}
+  
